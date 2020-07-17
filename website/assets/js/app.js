5cdc6163daa1ad1bb243ac4a6fd961dbd0c4d4c9
Kus <petr.kus@thermofisher.com>
2020-07-18 00:48:56 +0200

    change

---
+++ website/assets/js/app.js
@@ -0,0 +1,17 @@
+// Set the configuration for your app
+  // TODO: Replace with your project's config object
+  var config = {
+    apiKey: "AIzaSyDRaZcA60yey3fJ07FGh0xRwLaNkF_MP08",
+    authDomain: "noname-459cb.firebaseapp.com",
+    databaseURL: "https://noname-459cb.firebaseio.com/",
+    storageBucket: "noname-459cb.appspot.com"
+  };
+  firebase.initializeApp(config);
+
+  //get an element
+  var object = documnet.getElementById('object');
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+  object.innerText = database.ref().child('object');
