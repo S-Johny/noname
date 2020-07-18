5c4ce131a209cad47b14e9115b815d68b4e4ef21
Kus <petr.kus@thermofisher.com>
2020-07-18 02:59:46 +0200

    adding counting down to the table

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -1,5 +1,48 @@
+function zeroPad(num, size) 
+{
+    var s = "000000000" + num;
+    return s.substr(s.length-size);
+}
+
+function CountDown(id,running,starttime,time) 
+{
+	time_start= starttime;
+	time_now= parseInt((new Date().getTime()) / 1000) ;
+	
+	id_countdown = "time_" + id;
+	var countdown = document.getElementById(id_countdown);
+	
+	// find the amount of "seconds" between now and target
+	if (running == "true")
+	{
+		seconds_left = cash - (time_now - time_start);
+		if (seconds_left <=0)
+		{
+			seconds_left = 0;
+		}	
+	}
+	else
+	{
+		seconds_left = time;	
+	}
+	
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+    
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+    
+    minutes = parseInt(seconds_left / 60);
+    seconds_left = parseInt(seconds_left % 60);
+
+    seconds = seconds_left;
+    // format countdown string + set tag value
+    countdown.innerHTML = zeroPad(days, 3) + ":" + zeroPad(hours, 2) + ":" + zeroPad(minutes, 2) + ":" + zeroPad(seconds, 2);
+}
+
 // Set the configuration for your app
-  // TODO: Replace with your project's config object
+// TODO: Replace with your project's config object
   var config = {
     apiKey: "AIzaSyDRaZcA60yey3fJ07FGh0xRwLaNkF_MP08",
     authDomain: "noname-459cb.firebaseapp.com",
@@ -17,13 +60,15 @@
   query.once("value")
   .then(function(snapshot) { 
 	snapshot.forEach(function(user) {
+	  var id_number = 0
 	  var content = '';
       var value = user.val();
 	  content +='<tr>';
       content += '<td>' + value.name + '</td>';
-      content += '<td>' + value.time + '</td>';
+      content += "<td id='time_" + id_number + "'><script>setInterval(function (){CountDown(" + id_number + ",'" + value.running + "'," + value.starttime +"," + value.time +")}, 1000);</script></td>";
       content += '<td>' + value.team + '</td>';
       content += '</tr>';
       $('#user_table').append(content);
+      id_number++
   });
 });
