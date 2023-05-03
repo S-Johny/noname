f1f7021e822799ed32fc5fb95f199834acacf8cd
Johny <honstrap@gmail.com>
2023-05-03 19:48:25 +0200

    players layout change + time fixes

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -53,7 +53,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
   }
 
   calcDateDiff(
-    endDay: Date = new Date(2023, 4, 5, 6, 0, 0, 0),
+    endDay: Date = new Date('2023-05-04T04:00:00Z'),
   ): timeComponents {
     const dDay = endDay.valueOf();
 
