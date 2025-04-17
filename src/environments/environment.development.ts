8714d02528a9dc4dd19907401b3c1a922ad8e631
Jan Singon Slany <singond@seznam.cz>
2025-04-18 00:44:53 +0200

    Setup testing instance in separate Firebase app

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBKAJugddjpZvxwnzIVrgqYP-i_th5umgk",
+    authDomain: "noname-459cb.firebaseapp.com",
+    databaseURL: "https://noname-459cb.firebaseio.com",
+    projectId: "noname-459cb",
+    storageBucket: "noname-459cb.firebasestorage.app",
+    messagingSenderId: "795449313701",
+    appId: "1:795449313701:web:b794b04f38a0a3a4c4c04c",
+    measurementId: "G-5TB2BRSB1Y"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDRaZcA60yey3fJ07FGh0xRwLaNkF_MP08",
+    authDomain: "noname-459cb.firebaseapp.com",
+    databaseURL: "https://noname-459cb.firebaseio.com",
+    projectId: "noname-459cb",
+    storageBucket: "noname-459cb.firebasestorage.app",
+    messagingSenderId: "795449313701",
+    appId: "1:795449313701:web:1b11e5f95ce2cd28c4c04c",
+    measurementId: "G-GYKZ6WZ1Y2"
+  }
+};
