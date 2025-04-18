edd5abadb406bf2d9bc87484620d669796f9c0d6
Jan Singon Slany <singond@seznam.cz>
2025-04-18 19:12:15 +0200

    Fix time formatting in players table

--- src/app/shared/time-countdown.pipe.ts
+++ src/app/shared/time-countdown.pipe.ts
@@ -1,13 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Observable, interval, map } from 'rxjs';
-import { formatTime } from './utils';
+import { ConfigService } from './config.service';
+import { formatRemainingTime } from './utils';
 
 @Pipe({
   name: 'timeCountdown',
   standalone: false,
 })
 export class TimeCountdownPipe implements PipeTransform {
+  eventStart : number;
+
+  constructor(config : ConfigService) {
+    this.eventStart = config.getEventStart().getTime() / 1000;
+  }
+
   transform(gameTime: number): Observable<string> {
-    return interval(1000).pipe(map(() => formatTime(gameTime)));
+    var time = this.epochTime(gameTime);
+    return interval(1000).pipe(map(() => formatRemainingTime(time)));
+  }
+
+  epochTime(gameTime: number) {
+    return this.eventStart + gameTime;
   }
 }
